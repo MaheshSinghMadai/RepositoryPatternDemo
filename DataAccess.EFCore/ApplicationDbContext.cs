3cd0e6a27c45a83b4667642269d0ba46bc2c5fb8
corion <corion.pradhan@nestrait.com>
2022-03-28 11:47:28 +0545

    feat : test #1

--- DataAccess.EFCore/ApplicationDbContext.cs
+++ DataAccess.EFCore/ApplicationDbContext.cs
@@ -1,15 +1,17 @@
-﻿using Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+using Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain
+namespace DataAccess.EFCore
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
         public DbSet<Developer> Developers { get; set; }
         public DbSet<Project> Projects { get; set; }
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using DataAccess.EFCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Domain;
 
 namespace WebApi
 {
